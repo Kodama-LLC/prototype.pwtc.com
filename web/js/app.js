8880b881b091c223c625e2749b8831ed10928ef9
diaboloshogunate <rparker@kodama.design>
2017-05-30 11:15:36 -0700

    final changes

--- web/js/app.js
+++ web/js/app.js
@@ -6,3 +6,33 @@ $('.slick').slick({
     speed: 250,
     dots: true
 });
+$('.scroll-to-contact').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(".footer form").offset().top
+    }, 2000, function() {
+        $(".footer form [type='email']").focus();
+    });
+});
+var is_prev_days_shown = false;
+$('.toggle-previous-days').click(function(){
+    $('.day.previous').toggle();
+    is_prev_days_shown = !is_prev_days_shown;
+    
+    if(is_prev_days_shown) {
+        $(this).html("Hide previous days in this month");
+    } else {
+        $(this).html("Show previous days in this month");
+    }
+});
+$(window).on("scroll", function(){
+    if($(document).scrollTop() > $(window).height()) {
+        $('a.scroll-to-top').fadeIn();
+    } else {
+        $('a.scroll-to-top').fadeOut();
+    }
+});
+$('a.scroll-to-top').on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
